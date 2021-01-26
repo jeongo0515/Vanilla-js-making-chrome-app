7582dfe7cdfb2d02d67c2c9da3e4abb553f6de51
jeongo0515 <jjunjeongoo@naver.com>
2021-01-26 18:23:47 +0900

    making to do list

--- gretting.js
+++ gretting.js
@@ -5,6 +5,23 @@ const form = document.querySelector(".js-form"),
 const USER_LS = "currentUser",
     SHOWING_CN = "showing";
 
+function saveName(text) {
+    localStorage.setItem(USER_LS, text);
+}
+
+function handleSumit(event) {
+    event.preventDefault();
+    const currentValue = input.value;
+    console.log(currentValue);
+    paintGreeting(currentValue);
+    saveName(currentValue);
+}
+
+function askForName() {
+    form.classList.add(SHOWING_CN);
+    form.addEventListener("submit", handleSumit)
+}
+
 function paintGreeting(text) {
     form.classList.remove(SHOWING_CN);
     greeting.classList.add(SHOWING_CN);
@@ -14,7 +31,7 @@ function paintGreeting(text) {
 function loadName() {
     const currentUser = localStorage.getItem(USER_LS);
     if (currentUser === null) {
-        // 사용자가 있는 경우
+        askForName();
     } else {
         paintGreeting(currentUser); // 사용자가 있는 경우
     }
